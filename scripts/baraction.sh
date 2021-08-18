5af38796ce4007c1ae464b4f778e7c982e3a2fd9
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-08-18 14:18:25 -0500

    add colors, modules for icons, improve layout

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -10,27 +10,37 @@ hostname="${HOSTNAME:-${hostname:-$(hostname)}}"
 #	    DISK
 ##############################
 
+hddicon() {
+    echo ""
+}
+
 hdd() {
-	  hdd="$(df -h /home | grep /dev | awk '{print $3 " / " $5}')"
-        echo "ﴤ $hdd"
+	  free="$(df -h /home | grep /dev | awk '{print $3}' | sed 's/G/Gb/')"
+      perc="$(df -h /home | grep /dev/ | awk '{print $5}')"
+      echo "$perc  ($free)"
     }
 ##############################
 #	    RAM
 ##############################
+memicon() {
+    echo ""
+}
 
 mem() {
 used="$(free | grep Mem: | awk '{print $3}')"
 total="$(free | grep Mem: | awk '{print $2}')"
+human="$(free -h | grep Mem: | awk '{print $3}' | sed s/i//g)"
 
-totalh="$(free -h | grep Mem: | awk '{print $2}' | sed 's/Gi/G/')"
+ram="$(( 200 * $used/$total - 100 * $used/$total ))% ($human) "
 
-ram="$(( 200 * $used/$total - 100 * $used/$total ))% / $totalh "
-
-echo "  $ram"
+echo "$ram"
 }
 ##############################
 #	    CPU
 ##############################
+cpuicon() {
+    echo ""
+}
 
 cpu() {
 	  read cpu a b c previdle rest < /proc/stat
@@ -39,65 +49,80 @@ cpu() {
 	        read cpu a b c idle rest < /proc/stat
 		  total=$((a+b+c+idle))
 		    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-            echo " $cpu%"
+            echo "$cpu%"
 	      }
 ##############################
 #	    VOLUME
 ##############################
-
-vol() {
+volicon() {
     VOLONOFF="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $6}')"
-    VOL="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $5}')"
 
-    VOLICON="  "
-    MUTEICON=" 婢 "
+    VOLON="蓼"
+    VOLOFF="遼"
 
     if [ "$VOLONOFF" = "on" ]; then
-        echo "$VOLICON $VOL"
+        echo "$VOLON"
     else
-        echo "$MUTEICON"
+        echo "$VOLOFF"
     fi
+}
+
+vol() {
+    VOL="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $5}')"
+
+    echo "$VOL"
 }
 ##############################
 #	    Packages
 ##############################
+pkgicon() {
+    echo ""
+}
 
 pkgs() {
 	pkgs="$(dpkg -l | grep -c ^i)"
-	echo " $pkgs"
+	echo "$pkgs"
 }
 ##############################
 #	    UPGRADES
 ##############################
+upgradeicon() {
+    	echo ""
+}
 
 upgrades(){
 	upgrades="$(aptitude search '~U' | wc -l)"
-	echo " $upgrades"
+	echo "$upgrades"
 }
 ##############################
 #	    NETWORK
 ##############################
-
-network() {
+networkicon() {
 wire="$(ip a | grep enp | grep inet | wc -l)"
 wifi="$(ip a | grep wlan | grep inet | wc -l)"
 
 if [ $wire = 1 ]; then
-    echo "  $(hostname -I | awk '{print $1}')"
+    echo ""
 elif [ $wifi = 1 ]; then
-    echo "  $(hostname -I | awk '{print $1}')"
+    echo ""
 else
     echo "睊"
 fi
+
 }
 
-vpn() {
+ipaddress() {
+    address="$(ip a | grep 0.255 | grep -v wlp | cut -d ' ' -f6 | sed 's/\/24//')"
+    echo "$address"
+}
+
+vpnconnection() {
     state="$(ip a | grep tun0 | grep inet | wc -l)"
 
 if [ $state = 1 ]; then
     echo "ﱾ"
 else
-    echo ""
+    echo ""
 fi
 }
 
@@ -108,25 +133,26 @@ temp=$(cat ~/.config/weather.txt | grep temp_F | awk '{print $2}' | sed 's/\"//g
 
 nocloud="盛"
 cloud=""
-rain=""
-storm=""
 lotempicon="  "
 midtempicon="  "
 hitempicon="  "
 
 #print weather info
-
 TEMP() {
+    echo "$temp °"
+}
+
+tempicon() {
 if [ "$temp" -gt 80 ]; then
-	echo $hitempicon $temp F
+	echo $hitempicon
 elif [ "$temp" -ge 50 ] && [ "$temp" -le 79 ]; then
-	echo $midtempicon $temp F
+	echo $midtempicon
 elif [ "$temp" -le 49]; then
-	echo $lotempicon $temp F
+	echo $lotempicon
 fi
 }
 
-WEATHER () {
+weathericon() {
 if [[ "$weather" = "Clear" ]] || [[ "$weather" = "Sunny" ]]; then
 	echo "$nocloud $weather"
 else
@@ -157,10 +183,51 @@ fi
 #echo "$batstat  $battery %"
 #}
 
+clockicon() {
+    CLOCK=$(date '+%I')
+
+    case "$CLOCK" in
+    "00") icon="" ;;
+    "01") icon="" ;;
+    "02") icon="" ;;
+    "03") icon="" ;;
+    "04") icon="" ;;
+    "05") icon="" ;;
+    "06") icon="" ;;
+    "07") icon="" ;;
+    "08") icon="" ;;
+    "09") icon="" ;;
+    "10") icon="" ;;
+    "11") icon="" ;;
+    "12") icon="" ;;
+    esac
+
+    echo "$(date "+$icon")"
+}
+
+dateinfo() {
+    echo "$(date "+%b %d %Y (%a)")"
+}
+
+clockinfo() {
+    echo $(date "+%I:%M%p")
+}
+
+
       SLEEP_SEC=2
       #loops forever outputting a line every SLEEP_SEC secs
       while :; do
-          #echo "$(cpu) | $(mem) | $(pkgs) | $(upgrades) | $(hdd) | $(vpn) $(network) | $(vol) | $(WEATHER) $(TEMP) "
-          echo "+@bg=1; +@fg=0; $(cpu)  $(mem) +@bg=2; +@fg=1; $(pkgs)  $(upgrades) +@bg=3; $(hdd) +@bg=4; $(vpn) $(network) +@bg=5; $(vol) +@bg=6; $(WEATHER) $(TEMP) +@bg=1;+@fg=0;"
-		sleep $SLEEP_SEC
+        # echo "$(cpu) | $(mem) | $(pkgs) | $(upgrades) | $(hdd) | $(vpn) $(network) | $(vol) | $(WEATHER) $(TEMP) "
+        echo "+@fg=1; $(cpuicon) +@fg=0; $(cpu)\
+    +@fg=1; $(memicon) +@fg=0; $(mem)\
+    +@fg=2; $(pkgicon) +@fg=0; $(pkgs)\
+    +@fg=2; $(upgradeicon) +@fg=0; $(upgrades)\
+    +@fg=3; $(hddicon) +@fg=0; $(hdd)\
+    +@fg=4; $(networkicon) +@fg=0; $(ipaddress) +@fg=4; $(vpnconnection)\
+    +@fg=5; $(volicon) +@fg=0; $(vol)\
+    +@fg=6; $(weathericon) $(tempicon) $(TEMP)\
+    +@fg=1; $(dateinfo) +@fg=4; $(clockicon) +@fg=0; $(clockinfo)\
+    "
+        sleep $SLEEP_SEC
 		done
+
